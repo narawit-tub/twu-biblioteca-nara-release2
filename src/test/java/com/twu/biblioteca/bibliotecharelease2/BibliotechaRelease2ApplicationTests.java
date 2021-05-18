18ce7a33432a6ae7b799306e46e9765f823469a6
Narawit Tubtimtoe <narawit.tub@outlook.co.th>
2021-05-18 13:51:30 +0700

    :tada: initial commit

---
+++ src/test/java/com/twu/biblioteca/bibliotecharelease2/BibliotechaRelease2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.twu.biblioteca.bibliotecharelease2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotechaRelease2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
