f99b1b7d63a9ead678da408e9d659a4368833d54
Narawit Tubtimtoe <narawit.tub@outlook.co.th>
2021-05-21 13:15:21 +0700

    {2.3} :recycle: refactor: change how to login userService

--- src/main/java/com/twu/biblioteca/bibliotecharelease2/services/UserService.java
+++ src/main/java/com/twu/biblioteca/bibliotecharelease2/services/UserService.java
@@ -3,17 +3,40 @@ package com.twu.biblioteca.bibliotecharelease2.services;
 import com.twu.biblioteca.bibliotecharelease2.domain.UserApp;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Map;
 
 @Service
 public class UserService {
     private UserApp currentUser;
 
+    private ArrayList<UserApp> libarianList;
+    private ArrayList<UserApp> guestList;
+
+    public UserService() {
+        // user credential hardcode
+        libarianList = new ArrayList<>();
+        guestList = new ArrayList<>();
+        libarianList.add(new UserApp("Narawit", "Tubtimtoe", "nara@email.com", "0912345678", "libarian"));
+        guestList.add(new UserApp("Narawit", "Tubtimtoe", "nara@email.com", "0912345678", "guest"));
+    }
+
     public UserApp login (Map<String, String> userloginPayload) {
         // hardcode
-        if (userloginPayload.get("email").equals("nara@email.com") && userloginPayload.get("password").equals("1234") && userloginPayload.get("libaryNumber").equals("123-4567")) {
-            currentUser = new UserApp("Narawit", "Tubtimtoe", "nara@email.com", "0912345678", "libarian");
+        if (userloginPayload.get("libaryNumber") != null) {
+            for (UserApp libarian: libarianList) {
+                if (userloginPayload.get("email").equals("nara@email.com") && userloginPayload.get("password").equals("1234") && userloginPayload.get("libaryNumber").equals("123-4567")) {
+                    currentUser = libarian;
+                }
+            }
+        } else {
+            for (UserApp guest: guestList) {
+                if (userloginPayload.get("email").equals("nara@email.com") && userloginPayload.get("password").equals("1234") && userloginPayload.get("libaryNumber").equals("123-4567")) {
+                    currentUser = guest;
+                }
+            }
         }
+
         return currentUser;
     }
 
